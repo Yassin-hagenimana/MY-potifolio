2992fb9d000419827f4f612626526e89ba64e7be
yassin <hyassin509@gmail.com>
2021-06-05 11:44:50 +0200

    initial

---
+++ Vue Potifolio/potifolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <Portifolio />
+  </div>
+</template>
+
+<script>
+import Portifolio from './components/potifolio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Portifolio
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
